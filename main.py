d956990010ac6f0870aad8191c44759fed6834f1
leitoland95 <leitoland95@gmail.com>
2026-03-30 20:46:54 -0400

    Actualización automática de main.py desde Termux

--- main.py
+++ main.py
@@ -165,9 +165,9 @@ def signup_gmail():
         log("Clicando BTN")
         next_elem.click()
         #$$$$$$$$$$$$$$
-        
+        time.sleep(5)
         log("Obteniendo Select Gmail")
-        sel_prop = WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, "/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/c-wiz[1]/main[1]/div[2]/div[1]/div[1]/div[1]/span[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]")))
+        sel_prop = driver.find_element(By.XPATH, "/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/c-wiz[1]/main[1]/div[2]/div[1]/div[1]/div[1]/span[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]")
         log("Clicando Select")
         sel_prop.click()
         ######
